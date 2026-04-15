6ea2f38dd3817a345c35d906e67dc1da0913565a
Richard Freitag <freitag@sunet.se>
2026-04-15 21:33:57 +0100

    Update checksums and check for ssh key in prepareimage script

--- BerryPiGateway/prepareimage.sh
+++ BerryPiGateway/prepareimage.sh
@@ -4,6 +4,7 @@ target=$1
 baseurl=https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2026-04-14/
 imagefile=2026-04-13-raspios-trixie-arm64-lite.img.xz
 imageChecksum=5c9caff670594eb43b68afee2a156198cb4e4f58e5dec724b4520c53c0ab5aba  # Checksum of the uncompressed image
+uncompressedChecksum=5da77e33b60407a0861bd90a0ab2822214f18d3e0e66ad98fbe2674743dd81b1
 
 # Delete the local cache file until https://github.com/raspberrypi/rpi-imager/issues/974 is fixed
 if [ -f ~/.cache/Raspberry\ Pi/Imager/lastdownload.cache ]; then
@@ -16,7 +17,7 @@ if [ -f $baseurl$imagefile.sha256 ]; then
 fi
 wget $baseurl$imagefile.sha256 -O $imagefile.sha256
 
-if [ ! -f 2024-07-04-raspios-bookworm-arm64-lite.img.xz ]; then
+if [ ! -f $imagefile ]; then
     echo "(Re-)Downloading image!"
     wget $baseurl$imagefile
 fi
@@ -27,4 +28,13 @@ if ! [ "$(sha256sum -c $imagefile.sha256 | awk '{print $2}')" = "OK" ]; then
     exit 1
 fi
 
-rpi-imager --cli --sha256 $imageChecksum --first-run-script firstrun.sh $imagefile $target
+ssh_key=$(printf '%s\n' "$(cat sshkey.pub)" | sed 's/[&/\]/\\&/g')
+
+# Check if ssh_key is empty
+if [[ -z "$ssh_key" ]]; then
+  echo "Error: ssh_key is empty. Please provide a valid SSH key in a file named sshkey.pub." >&2
+  exit 1
+fi
+sed -i "s/SSH_KEY/$ssh_key/g" firstrun.sh
+
+rpi-imager --cli --sha256 $uncompressedChecksum --first-run-script firstrun.sh $imagefile $target
